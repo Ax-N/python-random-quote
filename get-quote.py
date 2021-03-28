4286ba6357686d1a5bd7ecf1d1dd399e009972ed
Joshua <okwese0@gmail.com>
2021-03-27 20:26:55 -0400

    Fixed the bug
    Uncommented lines 2 to 7

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
 import random
-#def master():
+def master():
    #print("Keep it logically awesome.")
 
- # f = open("quotes.txt")
- # quotes = f.readlines()
- # f.close()
-
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = len(quotes) - 1 
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
   #print(quotes[13])
   
 last = len(quotes) - 1 
