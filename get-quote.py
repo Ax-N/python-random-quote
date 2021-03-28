a7e5d2d5c390b1b8259fe35e0893784c1b7ae1a2
Joshua <okwese0@gmail.com>
2021-03-27 20:25:03 -0400

    Set lines 2 to 7 to comments
    Changed lines 2 to 7 to comments and Added lines 11 to 13

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,16 @@
-def master():
+import random
+#def master():
    #print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+ # f = open("quotes.txt")
+ # quotes = f.readlines()
+ # f.close()
 
-  print(quotes[13])
+  #print(quotes[13])
+  
+last = len(quotes) - 1 
+rnd = random.randint(0, last)
+print(quotes[rnd])
 
 if __name__== "__main__":
   master()
